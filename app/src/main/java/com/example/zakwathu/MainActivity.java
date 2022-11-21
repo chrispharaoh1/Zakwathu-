0114ef48c94af7db218ec745b772f27502ecb061
Chris <cpharaoh1@gmail.com>
2022-11-21 22:32:41 +0200

    User interface and layout

---
+++ app/src/main/java/com/example/zakwathu/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.zakwathu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.zakwathu.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+   ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new RadiosFragment());
+
+        binding.bottomNavigationView2.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.radio:
+                    replaceFragment(new RadiosFragment());
+                    break;
+
+                case R.id.tv:
+                    replaceFragment(new TvFragment());
+                    break;
+
+                case R.id.news:
+                    replaceFragment(new NewsFragment());
+                    break;
+
+
+            }
+
+            return  true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
